dab2ff4c7d8ea035b2ab6faea393fe1baf9dabf4
Haya AlMajali <hayamaj@Hayas-MacBook-Air.local>
2024-06-12 16:30:09 -0400

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,16 @@ export default async function Home() {
   const users: User[] = await fetchUsers();
 
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <div className="bg-[#1c1c1e] shadow-xl rounded-lg p-6 sm:w-full max-w-2xl">
+    <div className="min-h-screen bg-black text-white flex items-center justify-center p-4">
+      <div className="bg-[#1c1c1e] shadow-xl rounded-lg p-6 w-full max-w-2xl">
         <h1 className="text-3xl font-semibold mb-4">Users</h1>
         <ul className="divide-y divide-gray-700">
           {users.map(user => (
-            <li key={user.id} className="py-4 flex items-center justify-between">
-              <div className="flex-1 text-lg">
+            <li key={user.id} className="py-4 flex flex-col sm:flex-row items-center justify-between">
+              <div className="flex-1 text-lg text-center sm:text-left">
                 {user.name}
               </div>
-              <Link href={`/users/${user.id}`} className="text-blue-400 hover:text-blue-600 font-medium">
+              <Link href={`/users/${user.id}`} className="text-blue-400 hover:text-blue-600 font-medium mt-2 sm:mt-0">
                 View Details
               </Link>
             </li>
